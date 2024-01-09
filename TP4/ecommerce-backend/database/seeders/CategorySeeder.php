5b76a200d618b1576a2731a6d4247460addd0d34
MlleIlona <75298893+MlleIlona@users.noreply.github.com>
2024-01-09 15:00:05 +0700

    Update CategorySeeder.php

--- TP4/ecommerce-backend/database/seeders/CategorySeeder.php
+++ TP4/ecommerce-backend/database/seeders/CategorySeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        for($i=0;$i<10;$i++){
+            // $Category = categories::insert([
+            //     'name'=>Str::random(10),
+            // ]);
+            DB::table('category')->insert([
+                'name'=>Str::random(10),
+            ]);
+        }
     }
 }
